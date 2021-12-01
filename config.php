ad390cfc13f46a6f93af818675a11afbb05d3e7f
Protein Dispenser <60122610+MeidenWeiss@users.noreply.github.com>
2021-12-01 22:28:21 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 // XAMPP
-date_default_timezone_set("Asia/Manila");
-define('DB_SERVER','localhost');
-define('DB_USERNAME','root');
-define('DB_PASSWORD','');
-define('DB_DATABASE','db_misa');
+//date_default_timezone_set("Asia/Manila");
+//define('DB_SERVER','localhost');
+//define('DB_USERNAME','root');
+//define('DB_PASSWORD','');
+//define('DB_DATABASE','db_misa');
 
 // Heroku App
-//date_default_timezone_set("Asia/Manila");
-//define('DB_SERVER','us-cdbr-iron-east-04.cleardb.net');
-//define('DB_USERNAME','bd050aca9a448d');
-//define('DB_PASSWORD','ee32adac');
-//define('DB_DATABASE','heroku_8c53f4d1329000a');
+date_default_timezone_set("Asia/Manila");
+define('DB_SERVER','us-cdbr-iron-east-04.cleardb.net');
+define('DB_USERNAME','bd050aca9a448d');
+define('DB_PASSWORD','ee32adac');
+define('DB_DATABASE','heroku_8c53f4d1329000a');
 
 session_start();
